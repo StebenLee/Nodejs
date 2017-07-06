55c425556e3a8942512edce1a4000542a0a6edd9
JirLu <s1365789@gmail.com>
2017-07-06 20:50:27 +0800

    add illustrations

--- socketiochat/server.js
+++ socketiochat/server.js
@@ -7,13 +7,13 @@ const mongo = require('mongodb').MongoClient;
 const configDB = require('./config/database.js');
 
 const app = new express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //ejs template
 app.use(morgan('dev'));
 app.use(bodyParser());
 
-routes(app); // api
+routes(app); // api setup
 
 //socket io setting
 const server = http.createServer(app);
@@ -22,21 +22,22 @@ app.use('/static', express.static(__dirname + '/public'));
 server.listen(port, function() {
   console.log('listen on ' + port);
 });
+
 //io 
-io.on('connection', function(socket) { //user come in
+io.on('connection', function(socket) {
   
 
-  socket.on('add user', function(msg){
+  socket.on('add user', function(msg){ //user come in
     socket.username = msg;
     console.log("new user:"+msg+"logged.");
-    socket.broadcast.emit('add user' ,{
+    socket.broadcast.emit('add user' ,{ //broadcast to anyone else the sender
       username: socket.username
     });
   });
 
   mongo.connect(configDB.url, function(err, db) {
     let collection = db.collection('chat messages');
-    let stream = collection.find().stream(); //get messages
+    let stream = collection.find().stream(); //get all messages
     stream.on('data', function(chat) { socket.emit('chat', chat.name+" 說 : "+chat.content ); //io emit the message
 	}); 
   });
@@ -51,14 +52,16 @@ io.on('connection', function(socket) { //user come in
   socket.on('chat', function(msg) { //user enter message
     mongo.connect(configDB.url, function(err, db) {
 	    let collection = db.collection('chat messages');
-	    collection.insert({ name : socket.username , content : msg}, function(err, o) {
+	    collection.insert({ 
+      name : socket.username , content : msg}, //data schema
+      function(err, o) { //insert to db
 		  if (err) {
 		    console.log(err.message);
 		  } else { 
-		    console.log(socket.username +" inserted : "+ msg +" into db"); }
+		    console.log(socket.username +" inserted : "+ msg +" into db"); } //console log on CLI
 	    });
     });
-    socket.broadcast.emit('chat', socket.username+" 說 : "+msg);
+    socket.broadcast.emit('chat', socket.username+" 說 : "+msg); //broadcast to anyone else the sender
   });
 
 });
